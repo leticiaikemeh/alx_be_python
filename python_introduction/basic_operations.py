25744ecaa7b8de62215ad3c2f0075f47ea4cebb2
root <root@DESKTOP-F8D6VBB.localdomain>
2025-07-13 14:25:06 +0100

    0-

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diff = number1 - number2
+prod = number1 * number2
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", diff)
+print("Multiplication of", number1, "and", number2, "is", prod)
